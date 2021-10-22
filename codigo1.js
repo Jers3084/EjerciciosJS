943f50b831e90dae0c8b5f33277cc463030ac0b1
Jers3084 <jericrdez.c03@ucamp.utel.edu.mx>
2021-10-21 23:24:07 -0500

    actualizacion

---
+++ codigo1.js
@@ -0,0 +1,3 @@
+/*Codigo que escribe mensajes en consola*/
+console.log("Hola Mundo!"); //escribe en consola "Hola Mundo"
+console.log("Soy el primer script"); //escribe en consola "Soy el primer script"
---
+++ codigo2.js
@@ -0,0 +1,5 @@
+/*Codigo que escribe mensajes en consola*/
+console.log("Hola Mundo!"); //escribe en consola "Hola Mundo"
+console.log("Soy el primer script"); //escribe en consola "Soy el primer script"
+var mensaje="Hola Mundo!\n Qué facil es incluir \'comillas simples\' y \"comillas dobles\""
+alert(mensaje);
